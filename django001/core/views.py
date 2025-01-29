a8ac524ff507e21a3503d14e9e41488c46f41529
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2025-01-29 12:34:28 -0300

    Continuando os estudos depois de um tempo parado

---
+++ django001/core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request): #aqui ele vai requisitar e fazer a renderização da requizição + o arquivo index.py
+    context = {
+        'curso': "Web progamming with Django Framework",
+        'outro': "Django é massa"
+    }
+    return render(request, 'index.html', context)
+
+def contato(request):
+    return render(request, 'contato.html')
